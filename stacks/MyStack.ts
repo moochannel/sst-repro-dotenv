7ed2ac6b373d76ac536a49a40b2720a053bc9405
Katsuhisa Ueda <moochannel@gmail.com>
2023-02-18 00:00:11 +0900

    Place dotenv files and show the value on building MyStack

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,6 +1,8 @@
 import { StackContext, Api } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
+  console.log(`The value of environment variable KEY_A: ${process.env.KEY_A}`);
+
   const api = new Api(stack, "api", {
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
